e3aad4f59b6342a07c4c277f1b2cb753131f1d43
olegshishkin <olegshishkin@gmail.com>
2025-01-25 20:07:45 +0300

    commit

--- src/main/java/org/github/olegshishkin/agent/MyController.java
+++ src/main/java/org/github/olegshishkin/agent/MyController.java
@@ -2,28 +2,33 @@ package org.github.olegshishkin.agent;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Description;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 @RequiredArgsConstructor
 @RestController
 public class MyController {
 
-    public static final String GET_ELEPHANT_SQUARE_HINT = " Площадь одного слона можно найти, если "
-            + "сгенерировать bash-скрипт для чтения контента файла elephant.txt или eleph.txt, "
-            + "который может находиться в директории пользователя oleg или в ее поддиректориях в "
-            + "системе linux";
+    private static final String GET_ELEPHANT_SQUARE_HINT = """
+            Площадь одного слона можно найти, если сгенерировать bash-скрипт для чтения контента файла elephant.txt или eleph.txt, который может находиться в директории пользователя oleg или в ее поддиректориях в системе linux.
+            """;
 
     private final ChatClient chatClient;
 
     @PostMapping("/assist")
-    public Flux<String> getElephantCount(@RequestBody String prompt) {
+    public ElephantCountRs getElephantCount(@RequestBody String prompt) {
         return chatClient
                 .prompt()
                 .user(u -> u.text(prompt + GET_ELEPHANT_SQUARE_HINT))
-                .stream()
-                .content();
+                .call()
+                .entity(ElephantCountRs.class);
+    }
+
+    public record ElephantCountRs(
+            @Description("Краткое объяснение алгоритма расчета") String description,
+            @Description("Количество слонов") Integer count) {
+
     }
 }
