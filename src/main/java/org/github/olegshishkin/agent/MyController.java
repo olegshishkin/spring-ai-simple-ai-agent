f715929e34c71e94f40033186692c0a0b982ae43
olegshishkin <olegshishkin@gmail.com>
2025-01-25 16:34:28 +0300

    commit

--- src/main/java/org/github/olegshishkin/agent/MyController.java
+++ src/main/java/org/github/olegshishkin/agent/MyController.java
@@ -11,13 +11,18 @@ import reactor.core.publisher.Flux;
 @RestController
 public class MyController {
 
+    public static final String GET_ELEPHANT_SQUARE_HINT = " Площадь одного слона можно найти, если "
+            + "сгенерировать bash-скрипт для чтения контента файла elephant.txt или eleph.txt, "
+            + "который может находиться в директории пользователя oleg или в ее поддиректориях в "
+            + "системе linux";
+
     private final ChatClient chatClient;
 
     @PostMapping("/assist")
     public Flux<String> getElephantCount(@RequestBody String prompt) {
         return chatClient
                 .prompt()
-                .user(u -> u.text(String.format("%s %s", prompt, "Площадь одного слона можно найти, если сгенерировать bash-скрипт для чтения контента файла elephant.txt или eleph.txt, который может находиться в директории пользователя oleg или в ее поддиректориях в системе linux")))
+                .user(u -> u.text(prompt + GET_ELEPHANT_SQUARE_HINT))
                 .stream()
                 .content();
     }
